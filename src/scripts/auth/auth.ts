a0ddb366d1152829699a331acc73c98f6c4f225d
Rosario <rosario.polito@gmx.de>
2021-08-31 20:57:47 +0200

    login with forward to homepage working

--- src/scripts/auth/auth.ts
+++ src/scripts/auth/auth.ts
@@ -1,28 +1,44 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { FirebaseError } from "@firebase/util";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 export class Auth {
     private auth = getAuth();
 
     public createUserWithEmailAndPassword(email: string, password: string): void {
         createUserWithEmailAndPassword(this.auth, email, password)
-        .then((userCredential) => {
-            const { user: gottenUser } = userCredential;
-            console.log(gottenUser);
-        })
-        .catch((error: Error) => {
-            const authError = error as FirebaseError;
-            const {code: errorCode} = authError;
-            const {message: errorMessage} = authError;
-            
+            .then((userCredential) => {
+                const { user: gottenUser } = userCredential;
+                console.log(gottenUser);
+            })
+            .catch((error: Error) => {
+                const authError = error as FirebaseError;
+                const { code: errorCode } = authError;
+                const { message: errorMessage } = authError;
 
-            if(errorMessage === 'auth/weak-password'){
-                // eslint-disable-next-line no-alert
-                alert('TODO: Weak password from i18n');
-            }
 
-            return errorCode;
-        })
+                if (errorMessage === 'auth/weak-password') {
+                    // eslint-disable-next-line no-alert
+                    alert('TODO: Weak password from i18n');
+                }
+
+                return errorCode;
+            })
+    }
+
+    public async signInWithEmailAndPassword(email: string, password: string): Promise<void> {
+        // eslint-disable-next-line no-debugger
+        debugger;
+        await signInWithEmailAndPassword(this.auth, email, password)
+            .then((userCredential) => {
+                const { user: gottenUser } = userCredential;
+                console.log(gottenUser);
+            })
+            .catch((error: Error) => {
+                const authError = error as FirebaseError;
+                const { code: errorCode } = authError;
+
+                return errorCode;
+            })
     }
 }
--- src/views/VideoCourses.ts
+++ src/views/VideoCourses.ts
@@ -1,74 +1,56 @@
-import { LitElement, html, css } from 'lit';
-import { property } from 'lit/decorators.js';
-
-const logo = new URL('../../../assets/open-wc-logo.svg', import.meta.url).href;
+import { Router } from "@vaadin/router";
+import { css, html, LitElement, property } from "lit-element";
+import { Auth } from "../scripts/auth/auth.js";
 
 export class VideoCourses extends LitElement {
-  @property({ type: String }) title = 'My app';
 
-  static styles = css`
-    :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
-      text-align: center;
-      background-color: var(--video-courses-background-color);
+    @property({ type: String }) email = '';
+
+    @property({ type: String }) password = '';
+
+    static styles = css`
+    .mainWrapper{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    .inputWrapper{
+        display: flex;
+        flex-direction: column;
     }
 
-    main {
-      flex-grow: 1;
+    .input{
+        margin: 5px
+    }
+    /* Css comes here */
+`;
+
+    render() {
+        return html`
+    <!-- Html goes here -->
+    <main class="mainWrapper">
+        <h1>Login</h1>
+        <div class="inputWrapper">
+        <input class="input" id="user-name" type="email" @change="${this.updateEmail}" placeholder="Your username is your email address!" required>
+        <input class="input" id="password" type="password" @change="${this.updatePassword}" placeholder="Your password belongs here!"
+          required>
+      </div>
+      <button @click=${() =>{this.login()}}>Login</button>
+    </main>
+    `
     }
 
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
+    updateEmail(e: { target: HTMLInputElement }): void {
+        this.email = e.target.value
       }
-      to {
-        transform: rotate(360deg);
+    
+      updatePassword(e: { target: HTMLInputElement }): void {
+        this.password = e.target.value
       }
-    }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
-    }
-
-    .app-footer a {
-      margin-left: 5px;
-    }
-  `;
-
-  render() {
-    return html`
-      <main>
-      
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.title}</h1>
-      
-        <p>Edit <code>src/VideoCourses.ts</code> and save to reload.</p>
-        <a class="app-link" href="https://open-wc.org/guides/developing-components/code-examples" target="_blank"
-          rel="noopener noreferrer">
-          Code examples
-        </a>
-      </main>
-      
-      <p class="app-footer">
-        🚽 Made with love by
-        <a target="_blank" rel="noopener noreferrer" href="https://github.com/open-wc">open-wc</a>.
-      </p>
-      
-    `;
-  }
-
+    
+      async login(): Promise<void> {
+       const auth: Auth = new Auth();
+        auth.signInWithEmailAndPassword(this.email, this.password)
+        .then(() => Router.go('/home'));
+      }
 }
