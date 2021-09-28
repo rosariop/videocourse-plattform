2bbb9b80d960bef1bc4106bbb1a37190422eeb28
Rosario <rosario.polito@gmx.de>
2021-09-28 22:48:13 +0200

    feat: added purchased flag

--- stories/video-card.stories.ts
+++ stories/video-card.stories.ts
@@ -9,6 +9,7 @@ export default {
     title: 'lorem ipsum dolorem',
     sale: false,
     price: 99.99,
+    purchased: false
   },
 };
 
@@ -22,10 +23,11 @@ interface ArgTypes {
   title?: string;
   sale?: boolean;
   price?: number;
+  purchased?: boolean;
 }
 
-const Template: Story<ArgTypes> = ({ title, sale, price }: ArgTypes) => html`
-  <vid-card .sale=${sale} .price=${price} .title=${title}>
+const Template: Story<ArgTypes> = ({ title, sale, price, purchased }: ArgTypes) => html`
+  <vid-card .sale=${sale} .price=${price} .title=${title} .purchased=${purchased}>
     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
     eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
   </vid-card>
@@ -36,4 +38,5 @@ App.args = {
   title: 'Lorem ipsum dolor',
   price: 420.69,
   sale: false,
+  purchased: false
 };
